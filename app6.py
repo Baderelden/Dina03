2deaf7846e4104db8f6855d39fe967950cc0301c
Baderelden <82093696+Baderelden@users.noreply.github.com>
2025-01-06 22:18:06 +0000

    Update app6.py

--- app6.py
+++ app6.py
@@ -14,9 +14,9 @@ col1, col2, col3 = st.columns([3, 1, 1])
 with col1:
     st.title("KMMS AI Simulator")
 with col2:
-    st.image("logo.jpg", width=100)
+    st.image("logo.jpg", width=200)
 with col3:
-    st.image("logo2.jpg", width=150)
+    st.image("logo2.jpg", width=100)
 
 st.write('''
 Instructions:\n
