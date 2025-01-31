52f6697aaf8fa4474177c89ca7425d6e320bec58
Baderelden <82093696+Baderelden@users.noreply.github.com>
2025-01-31 00:02:31 +0000

    Update App9a.py

--- App9a.py
+++ App9a.py
@@ -20,7 +20,7 @@ def auto_play_audio(audio_file, ph2):
     with open(audio_file, "rb") as audio_file:
         audio_bytes=audio_file.read()
     base64_audio=base64.b64encode(audio_bytes).decode("utf-8")
-    audio_html = f'<audio src="data:audio/mp3;base64,{base64_audio}" controls autoply>'
+    audio_html = f'<audio src="data:audio/mp3;base64,{base64_audio}" controls autoplay>'
     ph2.markdown(audio_html, unsafe_allow_html=True)
         
 
