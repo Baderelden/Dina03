3cb42ac1901e8f957e2273f733d581a821e1e0ed
Baderelden <82093696+Baderelden@users.noreply.github.com>
2025-11-23 20:10:51 +0000

    Update Eval_v2.py

--- Eval_v2.py
+++ Eval_v2.py
@@ -268,7 +268,7 @@ def main():
         st.markdown(
             """
             <div style="display: flex; justify-content: flex-end; margin-bottom: 0.5rem;">
-                <img src="https://media.licdn.com/dms/image/v2/D4D0BAQH1uzgyZIvg8A/company-logo_200_200/company-logo_200_200/0/1712063501998/aasuedukw_logo?e=1765411200&v=beta&t=9TPSJcCdOJxwdFb126WDFFpFBBLVQ9rIMT2aiNySCrY" width="160">
+                <img src="https://media.licdn.com/dms/image/v2/D4D0BAQH1uzgyZIvg8A/company-logo_200_200/company-logo_200_200/0/1712063501998/aasuedukw_logo?e=1765411200&v=beta&t=9TPSJcCdOJxwdFb126WDFFpFBBLVQ9rIMT2aiNySCrY" width="120">
             </div>
             """,
             unsafe_allow_html=True,
